350e78a69eb12620ce5566ba8a4bf2fe6145d8ea
Ben Moran <github@benmoran.net>
2018-02-10 09:49:37 +0000

    Don't load images from disk until __getitem__ to save memory

--- pytorch/depiloader.py
+++ pytorch/depiloader.py
@@ -24,18 +24,16 @@ class DepiDataset(Dataset):
         for root, dirs, files in os.walk(rootdir):
             for filename in files:
                 if filename.endswith(".gz"):
-                    if substring is None or                         substring in filename:
+                    if substring is None or substring in filename:
                         yield os.path.join(root, filename)
 
     def __init__(self, datadir, substring=None, transform=None):
         self.filenames = list(self.find_files(datadir, substring))
         self.labels = [os.path.basename(filename)[8:11] for filename in self.filenames]
-        self.images = [nibabel.load(filename)
-                       for filename in self.filenames]
         self.transform = transform
     
     def __len__(self):
-        return len(self.images)
+        return len(self.filenames)
     
     def get_label(self, num):
         path = pathlib.Path(self.filenames[num])
@@ -43,17 +41,19 @@ class DepiDataset(Dataset):
             if part.startswith("subject_"):
                 return self.LABELS[part[8:11]]
         raise ValueError("Couldn't find label for {}".format(self.filenames[num]))
-    
+
+    def get_image(self, num):
+        filename = self.filenames[num]
+        return nibabel.load(filename)
+
     def __getitem__(self, num):
-        result = self.images[num].get_data(), self.get_label(num)
+        result = self.get_image(num).get_data(), self.get_label(num)
         if self.transform is not None:
             result = self.transform(result)
         return result
 
 
 # Creating some transformations.
-
-# In[112]:
 
 
 # I don't think we'll need this one, 
@@ -67,11 +67,8 @@ class Threshold(object):
         return np.where(np.abs(ndarray)>self.threshold_fraction, 
                         ndarray, 0), label
 
-
-# In[103]:
-
-
 class Normalize(object):
+    "Normalize the voxel data to have max of 1.0"
     def __call__(self, sample):
         ndarray, label = sample
         return ndarray / ndarray.max(), label
@@ -83,9 +80,6 @@ class AddChannel(object):
         tensor, label = sample
         tensor = tensor.contiguous()
         return tensor.view(torch.Size([1]) + tensor.shape), label
-    
-
-# In[104]:
 
 
 class ToTensor(object):
